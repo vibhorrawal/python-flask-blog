39443a81ab49e25c9fbb02a7307aa3c8ca4fe5c7
Kartikei-12 <kartikeimittal@gmail.com>
2018-10-15 11:20:53 +0530

    Added flask dependencies and restructured application

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,33 @@
-#Importing Flask
-from flask import Flask
+#Flask and it's dependencies
+from flask import Flask, request, current_app
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from config import Config
 
-#Flask dependencies
-app = Flask(__name__)
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+migrate = Migrate()
+bootstrap = Bootstrap()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = 'Please log in to access this page.'
 
-from app import routes
 
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    moment.init_app(app)
+
+    return app
 

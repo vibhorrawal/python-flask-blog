7972a2c1d91fc27d3a3d7e15f577ff48ee1c1361
Kartikei-12 <kartikeimittal@gmail.com>
2018-10-15 21:10:53 +0530

    Added post table

--- app/models.py
+++ app/models.py
@@ -22,6 +22,8 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(240))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -33,9 +35,15 @@ class User(UserMixin, db.Model):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
+        """
+        Method to check correctness of password
+        """
         return check_password_hash(self.password_hash, password)
 
     def avatar(self, size):
+        """
+        Method to generate URL for User avatar
+        """
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
@@ -53,3 +61,20 @@ def load_user(id):
     Method to load User
     """
     return User.query.get(int(id))
+
+
+class Post(db.Model):
+    """
+    Relational table to store posts by user
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140), index=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+
+
+

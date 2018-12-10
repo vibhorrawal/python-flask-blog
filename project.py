7fca384ec384a4386fdb8a9afca130d15c6e814d
Kartikei-12 <kartikeimittal@gmail.com>
2018-12-10 11:35:45 +0530

    Debugging

--- project.py
+++ project.py
@@ -14,7 +14,7 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Message':Message}
-
+"""
 if __name__ == "__main__" and sys.argv[1]:
     if sys.argv[1] == 'run':
         app.run(threaded=True)
@@ -24,3 +24,4 @@ if __name__ == "__main__" and sys.argv[1]:
         pass
 else:
     app.run(threaded=True)
+"""
